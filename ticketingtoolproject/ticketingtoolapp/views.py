34ede2d0295a0ebf439dddeec81a2397593d4d0a
Sarim Uddin <bazequa.fatima@gmail.com>
2022-11-20 12:41:12 +0530

    projectadded

---
+++ ticketingtoolproject/ticketingtoolapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def employee(request):
+    return render(request,'employee.html')
+def manager(request):
+    return render(request,'manager.html')
+def admin(request):
+    return render(request,'admin.html')
